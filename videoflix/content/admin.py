d93f059d4361c71367df0cf6193a5d173b3a8d04
Vicent Botella Ferragud <vicentbotella.93@gmail.com>
2022-09-21 14:45:21 +0200

    Video model created and registered

--- videoflix/content/admin.py
+++ videoflix/content/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Video
 
 # Register your models here.
+
+class VideoAdmin(admin.ModelAdmin):
+    fields = ('title', 'description', 'created_at', 'video_file',)    
+    list_display = ('title', 'description', 'created_at', 'video_file',)
+   
+admin.site.register(Video, VideoAdmin)
--- videoflix/content/models.py
+++ videoflix/content/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from datetime import date
 
 # Create your models here.
+
+class Video(models.Model):
+    title = models.CharField(max_length = 30)
+    description = models.CharField(max_length = 400)
+    created_at = models.DateField(default = date.today) 
+    video_file = models.FileField(upload_to = 'videos', blank = True, null = True)
+
+    def __str__(self):
+        return self.title
