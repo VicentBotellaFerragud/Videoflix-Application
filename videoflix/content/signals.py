25ccd1614c7489a5d383fdce457390bbb74c05d9
Vicent Botella Ferragud <vicentbotella.93@gmail.com>
2022-09-22 14:24:32 +0200

    signals.py file and video_post_save/video_post_delete functions created

---
+++ videoflix/content/signals.py
@@ -0,0 +1,28 @@
+from genericpath import isfile
+import os
+from django.dispatch import receiver
+from .models import Video
+from django.db.models.signals import post_save, post_delete
+
+"""
+Called whenever a video is created or edited.
+"""
+@receiver(post_save, sender = Video)
+def video_post_save(sender, instance, created, **kwargs): 
+    
+    print("video was saved")
+
+    if created:
+        print("new video created!")
+
+"""
+Called whenever a video is deleted. It deletes the video file from the videos folder. Without this function video objects could be 
+deleted, however the deletion of these would not automatically delete their video files (which are stored in the videos folder, inside 
+the media folder).
+"""
+@receiver(post_delete, sender = Video)
+def video_post_delete(sender, instance, **kwargs): 
+    
+    if instance.video_file:
+        if os.path.isfile(instance.video_file.path):
+            os.remove(instance.video_file.path)
--- videoflix/videoflix/settings.py
+++ videoflix/videoflix/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'content',
+    'content.apps.ContentConfig',
 ]
 
 MIDDLEWARE = [
