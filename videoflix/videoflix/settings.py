5a01772a79509e39afcae38e40820e369d85a9a1
Vicent Botella Ferragud <vicentbotella.93@gmail.com>
2022-10-22 13:10:54 +0200

    import-export library installed static files added

--- videoflix/videoflix/settings.py
+++ videoflix/videoflix/settings.py
@@ -46,6 +46,8 @@ INSTALLED_APPS = [
     'debug_toolbar',
 
     'django_rq',
+
+    'import_export',
 ]
 
 RQ_QUEUES = {
@@ -181,6 +183,8 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
