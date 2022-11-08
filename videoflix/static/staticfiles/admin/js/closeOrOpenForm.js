6b17dc7593f0d03a279c71cc8811aff2be5adb93
Alexandru-Andrei Todor <at@interap.de>
2022-11-08 10:56:05 +0100

    add form added

---
+++ videoflix/static/staticfiles/admin/js/closeOrOpenForm.js
@@ -0,0 +1,9 @@
+//These are the functions for the Homepage where 
+//you can open and Close the form to add an Video
+function openForm(){
+    document.getElementById("id1").classList.add("show-add-video-form-div");
+}
+
+function closeForm(){
+    document.getElementById("id1").classList.remove("show-add-video-form-div");
+}
