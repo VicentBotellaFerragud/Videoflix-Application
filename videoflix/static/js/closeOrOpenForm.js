94107fcf7e18695ad21fd2384ebf8ea4b9d23147
Vicent Botella <vb@interap.de>
2022-11-13 13:47:09 +0100

    form style adapted to app style code reformatted

--- videoflix/static/js/closeOrOpenForm.js
+++ videoflix/static/js/closeOrOpenForm.js
@@ -1,9 +1,24 @@
 //Global variables:
 const form = document.getElementById("addVideoForm");
-const button = document.getElementById("addVideoButton");
+const openOrCloseFormButton = document.getElementById("openOrCloseFormbutton");
+const DefaultSrc = openOrCloseFormButton.getAttribute("default-src");
+const AlternativeSrc = openOrCloseFormButton.getAttribute("alternative-src");
+const addVideoButton = document.getElementById("addVideoButton");
 
-function showOrHideForm() {
+function openOrCloseForm() {
 
     form.classList.toggle("hidden");
     
+    if (form.className === "add-video-form hidden") {
+
+        addVideoButton.classList.add("remove-transition");
+        openOrCloseFormButton.setAttribute("src", DefaultSrc);
+
+    } else {
+
+        addVideoButton.classList.remove("remove-transition");
+        openOrCloseFormButton.setAttribute("src", AlternativeSrc);
+
+    }
+
 }
