a294756059d6680157ed041ced2673788b455de6
Vicent Botella <vb@interap.de>
2022-11-27 17:17:09 +0100

    test

--- videoflix/videoflix/urls.py
+++ videoflix/videoflix/urls.py
@@ -31,4 +31,4 @@ urlpatterns = [
     path('admin/', admin.site.urls, name = 'admin'),
     path('__debug__/', include('debug_toolbar.urls'), name = 'debug'),
     path('django-rq/', include('django_rq.urls'), name = 'django-rq'),
-] + staticfiles_urlpatterns() + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT) # + staticfiles_urlpatterns()
